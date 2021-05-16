72ff88710f06f8be58bd7f9fd0965bdda276120a
Estuardo2015 <eiv10@humboldt.edu>
2021-05-15 20:23:50 -0700

    add gitignore and changed input keys

--- modules/input.go
+++ modules/input.go
@@ -5,13 +5,13 @@ import (
 )
 
 func HandleInput(g *Game) {
-	if ebiten.IsKeyPressed(ebiten.KeyArrowUp) {
+	if ebiten.IsKeyPressed(ebiten.KeyW) {
 		g.Level.Player.Move(g.Level.Player.X, g.Level.Player.Y-1, g.Level.TileGrid)
-	} else if ebiten.IsKeyPressed(ebiten.KeyArrowDown) {
+	} else if ebiten.IsKeyPressed(ebiten.KeyS) {
 		g.Level.Player.Move(g.Level.Player.X, g.Level.Player.Y+1, g.Level.TileGrid)
-	} else if ebiten.IsKeyPressed(ebiten.KeyArrowLeft) {
+	} else if ebiten.IsKeyPressed(ebiten.KeyA) {
 		g.Level.Player.Move(g.Level.Player.X-1, g.Level.Player.Y, g.Level.TileGrid)
-	} else if ebiten.IsKeyPressed(ebiten.KeyArrowRight) {
+	} else if ebiten.IsKeyPressed(ebiten.KeyD) {
 		g.Level.Player.Move(g.Level.Player.X+1, g.Level.Player.Y, g.Level.TileGrid)
 	}
 }
